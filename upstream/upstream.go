33e13aaafed092f540cda971d314767616c61f24
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-08-11 09:08:16 +0900

    package upstream

--- upstream/upstream.go
+++ upstream/upstream.go
@@ -1,13 +1,15 @@
-// go-multiproxier / upstream.go
+// go-multiproxier/upstream
 //
 // MIT License Copyright(c) 2018 Hiroshi Shimamoto
 // vim:set sw=4 sts=4:
 //
 
-package main
+package upstream
 
 import (
     "container/list"
+    "crypto/tls"
+    "errors"
     "fmt"
     "io"
     "io/ioutil"
@@ -34,6 +36,114 @@ type Upstream struct {
     BlockHosts [](*webhost.BlockHost)
     //
     CertCheckInterval time.Duration
+}
+
+func certcheckThisConn(conn *net.TCPConn, done chan bool, c *connection.Connection) (error, bool) {
+    outer := c.Outproxy
+    msg := "CONNECT " + c.Domain + ":443 HTTP/1.0\r\n\r\n"
+    conn.Write([]byte(msg))
+    buf, err := outer.CheckConnect(conn, "certcheckThisConn")
+    if err != nil {
+	return err, true
+    }
+    err = connection.CheckConnectOK(string(buf))
+    if err != nil {
+	return errors.New("Server returns error:" + err.Error()), true
+    }
+
+    log.Println("start certcheck communication for " + c.Domain + " with " + outer.Addr)
+
+    client := tls.Client(conn, &tls.Config{ ServerName: c.Domain })
+    defer client.Close()
+
+    err = client.Handshake()
+    if err != nil {
+	return err, false // no penalty
+    }
+    log.Println("cert for " + c.Domain + " good")
+
+    getreq := "GET / HTTP/1.1\r\n"
+    getreq += "Host: " + c.Domain + "\r\n"
+    getreq += "User-Agent: curl/7.58.0\r\n"
+    getreq += "Accept: */*\r\n"
+    getreq += "\r\n"
+    client.Write([]byte(getreq))
+
+    buf = make([]byte, 4096)
+    client.SetReadDeadline(time.Now().Add(outer.Timeout))
+    n, err := client.Read(buf)
+    if n > 0 {
+	resp := string(buf[:n])
+	if strings.Index(resp, `<title>Attention Required! | Cloudflare</title>`) > 0 {
+	    return errors.New("Cloudflare detect"), false
+	}
+	if strings.Index(resp, `<script src="https://www.google.com/recaptcha/api.js" async defer></script>`) > 0 {
+	    return errors.New("Google detect"), false
+	}
+    } else {
+	if err != nil {
+	    return errors.New("waiting GET / response " + err.Error()), false
+	}
+	return errors.New("remote TLS connection closed"), false
+    }
+
+    // send done in background
+    go func() {
+	defer conn.Close()
+	log.Println("done certcheck for " + c.Domain + " ok")
+	done <- true
+    }()
+
+    return nil, false
+}
+
+func tryThisConn(conn *net.TCPConn, done chan bool, c *connection.Connection) (error, bool) {
+    outer := c.Outproxy
+    // send original CONNECT
+    c.ReqWriteProxy(conn)
+    buf, err := outer.CheckConnect(conn, "tryThisConn")
+    if err != nil {
+	return err, true
+    }
+    err = connection.CheckConnectOK(string(buf))
+    if err != nil {
+	return errors.New("Server returns error:" + err.Error()), false
+    }
+
+    log.Println("start communication for " + c.Domain + " with " + outer.Addr)
+
+    go func() {
+	defer conn.Close()
+	// start hijacking
+	lconn := c.Hijack()
+	defer lconn.Close()
+
+	lconn.Write(buf)
+
+	connection.Transfer(lconn, conn)
+
+	log.Println("done communication for " + c.Domain)
+
+	done <- true
+    }()
+
+    return nil, false
+}
+
+func makeClusterBlob(c *cluster.Cluster) string {
+    out := c.CertHost + "=" + c.Host.String() + "\n"
+    if c.CertOK != nil {
+	out += "check time:" + c.CertOK.Format(time.ANSIC) + "\n"
+    } else {
+	out += "bad cluster\n"
+    }
+    c.Lock()
+    for e := c.OutProxies.Front(); e != nil; e = e.Next() {
+	outer := e.Value.(*outproxy.OutProxy)
+	out += " " + outer.Line()
+    }
+    c.Unlock()
+    return out
 }
 
 func (up *Upstream)checkBlock(host string) bool {
