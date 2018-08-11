b7eb5a79c30da43ce41bac4b965e6e2642180584
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2018-08-11 11:36:25 +0900

    connection: CertCheck and Run

--- connection/connection.go
+++ connection/connection.go
@@ -7,6 +7,7 @@
 package connection
 
 import (
+    "crypto/tls"
     "fmt"
     "io"
     "log"
@@ -117,4 +118,96 @@ func (c *Connection)GetOutProxy() *outproxy.OutProxy {
 
 func (c *Connection)SetOutProxy(o *outproxy.OutProxy) {
     c.outproxy = o
+}
+
+func (c *Connection)CertCheck(conn net.Conn, done chan bool) (error, bool) {
+    outer := c.GetOutProxy()
+    msg := "CONNECT " + c.Domain() + ":443 HTTP/1.0\r\n\r\n"
+    conn.Write([]byte(msg))
+    buf, err := outer.CheckConnect(conn, "certcheckThisConn")
+    if err != nil {
+	return err, true
+    }
+    err = CheckConnectOK(string(buf))
+    if err != nil {
+	return fmt.Errorf("Server returns error: %v", err), true
+    }
+
+    log.Println("start certcheck communication for " + c.Domain() + " with " + outer.Addr)
+
+    client := tls.Client(conn, &tls.Config{ ServerName: c.Domain() })
+    defer client.Close()
+
+    err = client.Handshake()
+    if err != nil {
+	return err, false // no penalty
+    }
+    log.Println("cert for " + c.Domain() + " good")
+
+    getreq := "GET / HTTP/1.1\r\n"
+    getreq += "Host: " + c.Domain() + "\r\n"
+    getreq += "User-Agent: curl/7.58.0\r\n"
+    getreq += "Accept: */*\r\n"
+    getreq += "\r\n"
+    client.Write([]byte(getreq))
+
+    buf = make([]byte, 4096)
+    client.SetReadDeadline(time.Now().Add(outer.Timeout))
+    n, err := client.Read(buf)
+    if n > 0 {
+	resp := string(buf[:n])
+	if strings.Index(resp, `<title>Attention Required! | Cloudflare</title>`) > 0 {
+	    return fmt.Errorf("Cloudflare detect"), false
+	}
+	if strings.Index(resp, `<script src="https://www.google.com/recaptcha/api.js" async defer></script>`) > 0 {
+	    return fmt.Errorf("Google detect"), false
+	}
+    } else {
+	if err != nil {
+	    return fmt.Errorf("waiting GET / response %v", err), false
+	}
+	return fmt.Errorf("remote TLS connection closed"), false
+    }
+
+    // send done in background
+    go func() {
+	defer conn.Close()
+	log.Println("done certcheck for " + c.Domain() + " ok")
+	done <- true
+    }()
+
+    return nil, false
+}
+
+func (c *Connection)Run(conn net.Conn, done chan bool) (error, bool) {
+    outer := c.GetOutProxy()
+    // send original CONNECT
+    c.ReqWriteProxy(conn)
+    buf, err := outer.CheckConnect(conn, "tryThisConn")
+    if err != nil {
+	return err, true
+    }
+    err = CheckConnectOK(string(buf))
+    if err != nil {
+	return fmt.Errorf("Server returns error: %v", err), false
+    }
+
+    log.Println("start communication for " + c.Domain() + " with " + outer.Addr)
+
+    go func() {
+	defer conn.Close()
+	// start hijacking
+	lconn := c.Hijack()
+	defer lconn.Close()
+
+	lconn.Write(buf)
+
+	Transfer(lconn, conn)
+
+	log.Println("done communication for " + c.Domain())
+
+	done <- true
+    }()
+
+    return nil, false
 }
